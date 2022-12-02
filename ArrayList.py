034b0015d4507cc81771cad9f53dee22019527bf
Max Base <maxbasecode@gmail.com>
2022-12-02 15:05:22 +0100

    Adding more to class

--- ArrayList.py
+++ ArrayList.py
@@ -6,18 +6,6 @@
 #
 
 class ArrayList:
-    # -   `add` - Add an element to the end of the list
-    # -   `add_at` - Add an element at a specific index
-    # -   `remove` - Remove an element from the list
-    # -   `remove_at` - Remove an element at a specific index
-    # -   `get` - Get an element at a specific index
-    # -   `set` - Set an element at a specific index
-    # -   `size` - Get the size of the list
-    # -   `is_empty` - Check if the list is empty
-    # -   `contains` - Check if the list contains an element
-    # -   `index_of` - Get the index of an element
-    # -   `clear` - Clear the list
-    # -   `to_string` - Get a string representation of the list
     def __init__(self, size=10):
         self.size = size
         self.count = 0
@@ -29,20 +17,108 @@ class ArrayList:
     def is_full(self):
         return self.count == self.size
     
-    def insert(self, item):
+    def add(self, item):
         if self.is_full():
             raise Exception("List is full")
         self.items.append(item)
         self.count += 1
+
+    def add_at(self, index, item):
+        if index < 0 or index >= self.size:
+            raise Exception("Index out of range")
+        self.items.insert(index, item)
     
-    def remove(self, item):
-        if self.is_empty():
-            raise Exception("List is empty")
-        self.items.remove(item)
-        self.count -= 1
+    def add_first(self, item):
+        self.add_at(0, item)
     
-    def search(self, item):
+    def add_last(self, item):
+        self.add(item)
+    
+    def remove_at(self, index):
+        if index < 0 or index >= self.size:
+            raise Exception("Index out of range")
+        del self.items[index]
+    
+    def remove_first(self):
+        self.remove_at(0)
+    
+    def remove_last(self):
+        self.remove_at(self.count - 1)
+    
+    def remove(self):
+        self.remove_last()
+    
+    def set(self, index, item):
+        if index < 0 or index >= self.size:
+            raise Exception("Index out of range")
+        if index >= self.count:
+            self.count = index + 1
+        self[index] = item
+
+
+    def size(self):
+        return self.count
+    
+    def clear(self):
+        self.items = []
+        self.count = 0
+    
+    def to_string(self):
+        return str(self.items)
+    
+    def __repr__(self):
+        return str(self.items)
+    
+    def __contains__(self, item):
+        return self.contains(item)
+
+    def contains(self, item):
         return item in self.items
+
+    def index_of(self, item):
+        return self.items.index(item)
+    
+    def __eq__(self, other):
+        return self.items == other.items
+    
+    def __ne__(self, other):
+        return self.items != other.items
+    
+    def __lt__(self, other):
+        return self.items < other.items
+    
+    def __le__(self, other):
+        return self.items <= other.items
+    
+    def __gt__(self, other):
+        return self.items > other.items
+    
+    def __ge__(self, other):
+        return self.items >= other.items
+
+    def __add__(self, other):
+        return self.items + other.items
+    
+    def __iadd__(self, other):
+        self.items += other.items
+        return self
+    
+    def __isub__(self, other):
+        self.items -= other.items
+        return self
+
+    def __sub__(self, other):
+        return self.items - other.items
+    
+    def __mul__(self, other):
+        return self.items * other
+    
+    def __imul__(self, other):
+        self.items *= other
+        return self
+    
+    def __rmul__(self, other):
+        return self.items * other
     
     def get(self, index):
         if index < 0 or index >= self.count:
